d577606239384668319d151877328b71f3ccb510
Kevin McConnell <kevin@37signals.com>
2023-02-07 16:43:15 +0000

    WIP: figure out where the race condition is

--- test/system/broadcasts_test.rb
+++ test/system/broadcasts_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class BroadcastsTest < ApplicationSystemTestCase
   test "Message broadcasts Turbo Streams" do
     visit messages_path
-    subscribe_to_broadcasts
+    wait_for_stream_to_be_connected
 
     assert_broadcasts_text "Message 1", to: :messages do |text, target|
       Message.create(content: text).broadcast_append_to(target)
@@ -12,7 +12,7 @@ class BroadcastsTest < ApplicationSystemTestCase
 
   test "Message broadcasts with html: render option" do
     visit messages_path
-    subscribe_to_broadcasts
+    wait_for_stream_to_be_connected
 
     assert_broadcasts_text "Hello, with html: option", to: :messages do |text, target|
       Message.create(content: "Ignored").broadcast_append_to(target, html: text)
@@ -21,7 +21,7 @@ class BroadcastsTest < ApplicationSystemTestCase
 
   test "Users::Profile broadcasts Turbo Streams" do
     visit users_profiles_path
-    subscribe_to_broadcasts
+    wait_for_stream_to_be_connected
 
     assert_broadcasts_text "Profile 1", to: :users_profiles do |text, target|
       Users::Profile.new(id: 1, name: text).broadcast_append_to(target)
@@ -30,20 +30,15 @@ class BroadcastsTest < ApplicationSystemTestCase
 
   test "passing extra parameters to channel" do
     visit echo_messages_path
+    wait_for_stream_to_be_connected
 
-    assert_broadcasts_text "Hello, world!", to: :messages do
-      subscribe_to_broadcasts
-    end
+    assert_text "Hello, world!"
   end
 
   private
 
-  def subscribe_to_broadcasts
-    click_on "Start listening for broadcasts"
-
-    assert_no_button "Start listening for broadcasts"
-
-    assert_selector :element, "turbo-cable-stream-source", visible: false
+  def wait_for_stream_to_be_connected
+    assert_selector "turbo-cable-stream-source[connected]", visible: false
   end
 
   def assert_broadcasts_text(text, to:, &block)
